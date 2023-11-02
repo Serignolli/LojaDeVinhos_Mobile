4eff34c4381ebf2e93ade5f34328bcd9d0634a86
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2023-11-01 21:36:10 -0300

    Criação do repositório

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>MANOOOOO MUDOU AQUI</Text>
+      <StatusBar style="light" backgroundColor="#000" translucent={true}/>
     </View>
   );
 }
---
+++ src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native-web";
+
+export default function Home(){
+    return(
+        <View>
+            <Text>Página Home</Text>
+        </View>
+    );
+}
